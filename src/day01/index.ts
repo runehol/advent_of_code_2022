01bd7d82adc6d1df456b9dd566affab3d0d8db78
Rune Holm <rune.holm@gmail.com>
2022-12-01 09:19:11 +0100

    solved day 1

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -2,39 +2,44 @@ import run from "aocrunner";
 import _ from 'lodash'
 
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string) => 
+{
+    return rawInput.split("\n\n").map((s) => s.split("\n").map( (n) => parseInt(n)))
+}
 
 const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-
-  return;
+    const input = parseInput(rawInput);
+    const sums = input.map((arr) => _.sum(arr))
+    const max = _.max(sums);
+    return max;
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-
-  return;
+    const input = parseInput(rawInput);
+    const sums = input.map((arr) => _.sum(arr));
+    sums.sort((a, b) => b-a);
+    return _.sum(sums.slice(0, 3));
 };
 
 run({
-  part1: {
-    tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
-    ],
-    solution: part1,
-  },
-  part2: {
-    tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
-    ],
-    solution: part2,
-  },
-  trimTestInputs: true,
-  onlyTests: false,
+    part1: {
+        tests: [
+          // {
+          //   input: ``,
+          //   expected: "",
+          // },
+        ],
+        solution: part1,
+    },
+    part2: {
+        tests: [
+          // {
+          //   input: ``,
+          //   expected: "",
+          // },
+        ],
+        solution: part2,
+    },
+    trimTestInputs: true,
+    onlyTests: false,
 });
