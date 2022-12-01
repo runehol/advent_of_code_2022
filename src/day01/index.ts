f788431bddfe7506fbcefd363aabeba133507b84
Rune Holm <rune.holm@gmail.com>
2022-12-01 23:43:23 +0100

    formatted chains

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,18 +1,27 @@
 import run from "aocrunner";
-import _ from 'lodash'
+import _ from 'lodash';
 
 
 const parseInput = (rawInput: string) : number[][] => 
 {
-    return _(rawInput.split("\n\n")).map((s:string) => s.split("\n").map( (n:string) => parseInt(n))).value();
+    return (_(rawInput.split("\n\n"))
+    .map((s:string) => s.split("\n")
+    .map( (n:string) => parseInt(n)))
+    .value());
 }
 
 const part1 = (rawInput: string) : number => {
-    return _(parseInput(rawInput)).map((arr:number[]) => _.sum(arr)).max()||0;
+    return (_(parseInput(rawInput))
+    .map((arr:number[]) => _.sum(arr))
+    .max()||0);
 };
 
 const part2 = (rawInput: string) : number => {
-    return _(parseInput(rawInput)).map((arr) => _.sum(arr)).sort((a, b) => b-a).slice(0, 3).sum();
+    return (_(parseInput(rawInput))
+    .map((arr) => _.sum(arr))
+    .sort((a, b) => b-a)
+    .slice(0, 3)
+    .sum());
 };
 
 run({
