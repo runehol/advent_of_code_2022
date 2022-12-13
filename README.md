d91712dd3b6653faf131c4166b32928f189fd17d
Rune Holm <rune.holm@gmail.com>
2022-12-13 07:06:14 +0000

    solved day 13

--- README.md
+++ README.md
@@ -23,7 +23,7 @@
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
 [![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
-![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%85/green)](src/day13)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
@@ -146,16 +146,16 @@ Both parts: 115.199626ms
 
 ```
 Day 12
-Time part 1: 12.839ms
-Time part 2: 13.169ms
-Both parts: 26.007666999999998ms
+Time part 1: 10.919ms
+Time part 2: 7.316ms
+Both parts: 18.234458ms
 ```
 
 ```
 Day 13
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 2.188ms
+Time part 2: 2.34ms
+Both parts: 4.528124999999999ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 22/50
-Total time: 168.964ms
+Total stars: 24/50
+Total time: 165.719ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day13/index.ts
@@ -0,0 +1,133 @@
+import run from "aocrunner";
+import _ from 'lodash';
+
+interface Pair
+{
+    left: any;
+    right: any;
+    index: number;
+}
+
+const parseInput = (rawInput: string) : Pair[] => 
+{
+    return rawInput.split("\n\n").map((chunk, i) => 
+        {
+            let [left, right] = chunk.split("\n").map(line => eval(line));
+            const index = i+1;
+            return {left, right, index};
+        });
+}
+
+const promote_to_array = (v:number) => new Array(1).fill(v);
+
+const ordered = (left: any, right: any) : number =>
+{
+    const left_is_num = typeof left === 'number';
+    const right_is_num = typeof right === 'number';
+    if(left_is_num && right_is_num)
+    {
+        if(left < right) return -1;
+        if(left > right) return 1;
+        return 0;
+    }
+    if(left_is_num) return ordered(promote_to_array(left), right);
+    if(right_is_num) return ordered(left, promote_to_array(right));
+
+    //finally, both are arrays
+    for(let i = 0; i < Math.max(left.length, right.length); ++i)
+    {
+        const le = left[i];
+        const re = right[i];
+        if(typeof le === 'undefined') return -1;
+        if(typeof re === 'undefined') return 1;
+        const internal_order = ordered(le, re);
+        if(internal_order != 0) return internal_order;
+    }
+    return 0;
+
+}
+
+const part1 = (rawInput: string) => {
+    const input = parseInput(rawInput);
+
+    return _(input).filter(pair => {
+        return ordered(pair.left, pair.right) < 0
+    }).map(pair => pair.index).sum()
+};
+
+const part2 = (rawInput: string) => {
+    const input = parseInput(rawInput);
+    let packets = _(input).flatMap(pair => [pair.left, pair.right]).value();
+    let divider_a = [[2]];
+    let divider_b = [[6]];
+    packets.push(divider_a, divider_b);
+    packets.sort(ordered);
+    return (packets.indexOf(divider_a)+1)*(packets.indexOf(divider_b)+1);
+};
+
+run({
+    part1: {
+        tests: [
+            {
+                input: `[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]`,
+                expected: 13,
+            },
+        ],
+        solution: part1,
+    },
+    part2: {
+        tests: [
+            {
+                input: `[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]`,
+                expected: 140,
+            },
+        ],
+        solution: part2,
+    },
+    trimTestInputs: true,
+    onlyTests: false,
+});
